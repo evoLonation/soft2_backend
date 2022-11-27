1a7b50a3375b5c8bad0f2c69cbc7155ba52c4f1e
MuYu113 <LH2018710838@163.com>
2022-11-28 00:22:17 +0800

    UserDebug

---
+++ service/user/model/subscribemodel.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
+
+var _ SubscribeModel = (*customSubscribeModel)(nil)
+
+type (
+	// SubscribeModel is an interface to be customized, add more methods here,
+	// and implement the added methods in customSubscribeModel.
+	SubscribeModel interface {
+		subscribeModel
+	}
+
+	customSubscribeModel struct {
+		*defaultSubscribeModel
+	}
+)
+
+// NewSubscribeModel returns a model for the database table.
+func NewSubscribeModel(conn sqlx.SqlConn, c cache.CacheConf) SubscribeModel {
+	return &customSubscribeModel{
+		defaultSubscribeModel: newSubscribeModel(conn, c),
+	}
+}
