a7dad5d90ea626d39bbb6d9cd2da6ce88896cf48
MaxLong <longyizhou2001@163.com>
2022-11-27 22:00:56 +0800

    lyz: add paper logic

--- service/paper/api/internal/logic/scholarbasiclogic.go
+++ service/paper/api/internal/logic/scholarbasiclogic.go
@@ -1,9 +1,14 @@
 package logic
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"log"
 	"soft2_backend/service/paper/api/internal/svc"
 	"soft2_backend/service/paper/api/internal/types"
+	"soft2_backend/service/paper/database"
+	"sort"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -24,6 +29,64 @@ func NewScholarBasicLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Scho
 
 func (l *ScholarBasicLogic) ScholarBasic(req *types.ScholarBasicRequest) (resp *types.ScholarBasicResponse, err error) {
 	// todo: add your logic here and delete this line
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"id": req.ScholarId,
+			},
+		},
+	}
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Printf("Error encoding query: %s\n", err)
+	}
+	log.Println(buf.String())
+	res := database.SearchAuthor(buf)
 
+	source := res["hits"].(map[string]interface{})["hits"].([]interface{})[0].(map[string]interface{})["_source"].(map[string]interface{})
+	var tags []types.TagJSON
+	for _, tag := range source["tags"].([]interface{}) {
+		tags = append(tags, types.TagJSON{
+			T: tag.(map[string]interface{})["t"].(string),
+			W: int(tag.(map[string]interface{})["w"].(float64)),
+		})
+	}
+	var institutions []string
+	for _, institution := range source["orgs"].([]interface{}) {
+		institutions = append(institutions, institution.(string))
+	}
+	var years []int
+	for _, pub := range source["pubs"].([]interface{}) {
+		query = map[string]interface{}{
+			"query": map[string]interface{}{
+				"match": map[string]interface{}{
+					"id": pub.(map[string]interface{})["i"].(string),
+				},
+			},
+		}
+		if err := json.NewEncoder(&buf).Encode(query); err != nil {
+			log.Printf("Error encoding query: %s\n", err)
+		}
+		log.Println(buf.String())
+		res := database.SearchPaper(buf)
+		source := res["hits"].(map[string]interface{})["hits"].([]interface{})[0].(map[string]interface{})["_source"].(map[string]interface{})
+		if NilHandler(source["year"], "int") != nil {
+			years = append(years, NilHandler(source["year"], "int").(int))
+		}
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(years)))
+
+	resp = &types.ScholarBasicResponse{
+		ScholarId:    source["id"].(string),
+		Name:         source["name"].(string),
+		Institution:  institutions,
+		Position:     NilHandler(source["position"], "string").(string),
+		RefNum:       int(source["n_citation"].(float64)),
+		AchNum:       int(source["n_pubs"].(float64)),
+		HIndex:       int(source["h_index"].(float64)),
+		IsIdentified: int(source["is_identified"].(float64)) == 1,
+		Years:        years,
+		Tags:         tags,
+	}
 	return
 }
