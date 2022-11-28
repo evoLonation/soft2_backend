8f325d5bf5e05721db06b660afcb3c863230d12e
MaxLong <longyizhou2001@163.com>
2022-11-29 00:34:48 +0800

    elastic

--- service/paper/database/elastic.go
+++ service/paper/database/elastic.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/elastic/go-elasticsearch/v7"
+	"io"
 	"log"
 )
 
@@ -46,6 +47,23 @@ func SearchAuthor(query bytes.Buffer) map[string]interface{} {
 		es.Search.WithBody(&query),
 		es.Search.WithTrackTotalHits(true),
 		es.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Printf("Error getting response: %s\n", err)
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		log.Printf("Error parsing the response body: %s\n", err)
+	}
+	return res
+}
+
+func MgetPaper(query bytes.Buffer) map[string]interface{} {
+	var res map[string]interface{}
+	resp, err := es.Mget(
+		io.Reader(&query),
+		es.Mget.WithContext(context.Background()),
+		es.Mget.WithIndex("papers"),
+		es.Mget.WithPretty(),
 	)
 	if err != nil {
 		log.Printf("Error getting response: %s\n", err)
