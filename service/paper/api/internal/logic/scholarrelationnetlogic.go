c396a21c26e6b55458da22731922671bf23e5e4b
MaxLong <longyizhou2001@163.com>
2022-12-11 21:56:25 +0800

    fix net

--- service/paper/api/internal/logic/scholarrelationnetlogic.go
+++ service/paper/api/internal/logic/scholarrelationnetlogic.go
@@ -107,6 +107,7 @@ func (l *ScholarRelationNetLogic) ScholarRelationNet(req *types.ScholarRelationN
 		if pub.(map[string]interface{})["found"].(bool) == false {
 			continue
 		}
+
 		authors := pub.(map[string]interface{})["_source"].(map[string]interface{})["authors"].([]interface{})
 		for _, author := range authors {
 			authorId := NilHandler(author.(map[string]interface{})["id"].(string), "string").(string)
