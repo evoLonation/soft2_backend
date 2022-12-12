0b1f0e66e4cae57859d97211a8ced94d4ecf0881
MuYu113 <LH2018710838@163.com>
2022-12-12 20:40:29 +0800

    fix comment

---
+++ service/user/api/internal/handler/getcommenthandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"soft2_backend/service/user/api/internal/logic"
+	"soft2_backend/service/user/api/internal/svc"
+	"soft2_backend/service/user/api/internal/types"
+)
+
+func GetCommentHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.GetCommentRequest
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewGetCommentLogic(r.Context(), svcCtx)
+		resp, err := l.GetComment(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
