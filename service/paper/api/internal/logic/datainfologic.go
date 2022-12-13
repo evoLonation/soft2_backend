99ff331db22b5e8ce6dc894c4adae4fbdb8bb71c
MaxLong <longyizhou2001@163.com>
2022-12-13 22:50:19 +0800

    data info

--- service/paper/api/internal/logic/datainfologic.go
+++ service/paper/api/internal/logic/datainfologic.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"log"
+	"soft2_backend/service/paper/database"
 
 	"soft2_backend/service/paper/api/internal/svc"
 	"soft2_backend/service/paper/api/internal/types"
@@ -25,5 +29,39 @@ func NewDataInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DataInfo
 
 func (l *DataInfoLogic) DataInfo(req *types.DataInfoRequest) (resp *types.DataInfoResponse, err error) {
 	// todo: add your logic here and delete this line
-	return nil, nil
+	var paperBuf bytes.Buffer
+	paperQuery := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match_all": map[string]interface{}{},
+		},
+		"size":             0,
+		"track_total_hits": true,
+	}
+	if err := json.NewEncoder(&paperBuf).Encode(paperQuery); err != nil {
+		log.Printf("Error encoding query: %s", err)
+	}
+	log.Println(paperBuf.String())
+	paperRes := database.SearchPaper(paperBuf)
+	paperNum := int(paperRes["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64))
+
+	var scholarBuf bytes.Buffer
+	scholarQuery := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match_all": map[string]interface{}{},
+		},
+		"size":             0,
+		"track_total_hits": true,
+	}
+	if err := json.NewEncoder(&scholarBuf).Encode(scholarQuery); err != nil {
+		log.Printf("Error encoding query: %s", err)
+	}
+	log.Println(scholarBuf.String())
+	scholarRes := database.SearchAuthor(scholarBuf)
+	scholarNum := int(scholarRes["hits"].(map[string]interface{})["total"].(map[string]interface{})["value"].(float64))
+
+	resp = &types.DataInfoResponse{
+		PaperNum:   paperNum,
+		ScholarNum: scholarNum,
+	}
+	return resp, nil
 }
