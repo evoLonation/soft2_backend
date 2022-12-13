9f38195884398fef7278805260079be67237c38b
lenhhh <1249116871@qq.com>
2022-12-13 18:37:40 +0800

    测试 邮箱认证学者

--- service/apply/api/internal/logic/emailIdentifyLogic.go
+++ service/apply/api/internal/logic/emailIdentifyLogic.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"soft2_backend/service/apply/api/internal/svc"
 	"soft2_backend/service/apply/api/internal/types"
 	"soft2_backend/service/apply/model"
@@ -28,24 +27,20 @@ func NewEmailIdentifyLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Ema
 
 func (l *EmailIdentifyLogic) EmailIdentify(req *types.EmailIdentifyRequest) (resp *types.EmailIdentifyResponse, err error) {
 	verifyCode, err := l.svcCtx.VerifycodeModel.FindOne(l.ctx, req.Email)
-	fmt.Println("1111111111111111111111111111111")
 	if err != nil {
 		return &types.EmailIdentifyResponse{
 			Msg: "验证码错误!",
 		}, nil
 	}
-	fmt.Println("2222222222222222222222222222222")
 	if verifyCode.Code != req.VerifyCode {
 		return &types.EmailIdentifyResponse{
 			Msg: "验证码错误!",
 		}, nil
 	}
-	fmt.Println("3333333333333333333333333333333")
 	err = l.svcCtx.VerifycodeModel.Delete(l.ctx, req.Email)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("4444444444444444444444444444444")
 
 	userId, _ := l.ctx.Value("UserId").(json.Number).Int64()
 	newApply := model.Apply{
@@ -54,12 +49,10 @@ func (l *EmailIdentifyLogic) EmailIdentify(req *types.EmailIdentifyRequest) (res
 		ApplyType: 1,
 		Email:     sql.NullString{String: req.Email, Valid: true},
 	}
-	fmt.Println("5555555555555555555555555555555")
 	_, err = l.svcCtx.ApplyModel.Insert(l.ctx, &newApply)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println("6666666666666666666666666666666")
 	return &types.EmailIdentifyResponse{
 		Msg: "验证码正确!",
 	}, nil
